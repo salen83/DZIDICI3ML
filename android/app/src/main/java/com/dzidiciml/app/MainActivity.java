c012796dba8787bc275aee45c5d9672ac374df5e
Salen83 <dzidicdzejlan2@gmail.com>
2026-01-10 21:56:02 +0100

    Update MainActivity.java

--- android/app/src/main/java/com/dzidiciml/app/MainActivity.java
+++ android/app/src/main/java/com/dzidiciml/app/MainActivity.java
@@ -1,5 +1,29 @@
 package com.dzidiciml.app;
 
+import android.os.Bundle;
+import android.util.Log;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    private static final String TAG = "DZIDICIML";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // Globalni handler za crash-eve u Android delu
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            Log.e(TAG, "Uncaught exception in thread " + thread.getName(), throwable);
+        });
+
+        super.onCreate(savedInstanceState);
+
+        // Log kada se BridgeActivity startuje
+        Log.i(TAG, "MainActivity created - BridgeActivity started");
+
+        // Capacitor plugin registry log (opciono)
+        for (Plugin plugin : getBridge().getPluginManager().getPlugins()) {
+            Log.i(TAG, "Registered plugin: " + plugin.getId());
+        }
+    }
+}
