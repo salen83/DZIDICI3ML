d4c552d4eceb50304b48bbf608d52c42562c7972
Salen83 <dzidicdzejlan2@gmail.com>
2026-01-11 01:58:07 +0100

    Update MainActivity.java

--- android/app/src/main/java/com/dzidiciml/app/MainActivity.java
+++ android/app/src/main/java/com/dzidiciml/app/MainActivity.java
@@ -1,55 +1,53 @@
 package com.dzidiciml.app;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
+import org.json.JSONObject;
+
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.FileWriter;
 
 public class MainActivity extends BridgeActivity {
 
     private static final String TAG = "DZIDICIML";
-    private static final String LOG_FILE = "crash_log.txt";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        // Postavi globalni handler za sve neočekivane greške
+
+        // Globalni handler za sve crash-eve
         Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            try {
+                JSONObject json = new JSONObject();
+                json.put("thread", thread.getName());
+                json.put("message", throwable.getMessage());
+                json.put("stacktrace", Log.getStackTraceString(throwable));
+
+                File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "DZIDICIML");
+                if (!folder.exists()) folder.mkdirs();
+                File file = new File(folder, "crash_log.json");
+
+                FileWriter writer = new FileWriter(file, true); // dodaj na kraj fajla
+                writer.write(json.toString() + "\n");
+                writer.close();
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to write crash log", e);
+            }
+
             Log.e(TAG, "Uncaught exception in thread " + thread.getName(), throwable);
-            writeCrashToFile(throwable);
+
+            // Ovu liniju obavezno pozvati da sistem završi crash
+            System.exit(1);
         });
 
         super.onCreate(savedInstanceState);
 
-        // Log kada se BridgeActivity startuje
-        Log.i(TAG, "MainActivity created - BridgeActivity started");
-
-        // Opcionalno: loguj sve registrovane Capacitor plugine
+        // Opcionalno: log plugin-ova
         for (Plugin plugin : getBridge().getPluginManager().getPlugins()) {
             Log.i(TAG, "Registered plugin: " + plugin.getId());
-        }
-    }
-
-    // Upisuje stack trace u interni fajl
-    private void writeCrashToFile(Throwable throwable) {
-        try {
-            File file = new File(getFilesDir(), LOG_FILE);
-            FileOutputStream fos = new FileOutputStream(file, true); // append mode
-            PrintWriter pw = new PrintWriter(fos);
-            pw.println("----- CRASH at " + System.currentTimeMillis() + " -----");
-            StringWriter sw = new StringWriter();
-            PrintWriter stackPw = new PrintWriter(sw);
-            throwable.printStackTrace(stackPw);
-            pw.println(sw.toString());
-            pw.println("--------------------------------------------------");
-            pw.close();
-            fos.close();
-        } catch (Exception e) {
-            Log.e(TAG, "Failed to write crash to file", e);
         }
     }
 }
